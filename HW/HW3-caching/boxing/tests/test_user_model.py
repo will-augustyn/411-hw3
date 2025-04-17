ba6a9fad0d87f243c2f71cf3c2b8ba8b71853399
will-augustyn <will.augustyn0404@gmail.com>
2025-04-17 12:10:43 -0400

    uncommented all tests

--- HW/HW3-caching/boxing/tests/test_user_model.py
+++ HW/HW3-caching/boxing/tests/test_user_model.py
@@ -55,52 +55,52 @@ def test_check_password_user_not_found(session):
 # Update Password
 ##########################################################
 
-# def test_update_password(session, sample_user):
-#     """Test updating the password for an existing user."""
-#     Users.create_user(**sample_user)
-#     new_password = "newpassword456"
-#     Users.update_password(sample_user["username"], new_password)
-#     assert Users.check_password(sample_user["username"], new_password) is True
-def test_update_password(): return True
+def test_update_password(session, sample_user):
+    """Test updating the password for an existing user."""
+    Users.create_user(**sample_user)
+    new_password = "newpassword456"
+    Users.update_password(sample_user["username"], new_password)
+    assert Users.check_password(sample_user["username"], new_password) is True
+# def test_update_password(): return True
 
-# def test_update_password_user_not_found(session):
-#     """Test updating the password for a non-existent user."""
-#     with pytest.raises(ValueError, match="User nonexistentuser not found"):
-#         Users.update_password("nonexistentuser", "newpassword")
-def test_update_password_user_not_found(): return True
+def test_update_password_user_not_found(session):
+    """Test updating the password for a non-existent user."""
+    with pytest.raises(ValueError, match="User nonexistentuser not found"):
+        Users.update_password("nonexistentuser", "newpassword")
+# def test_update_password_user_not_found(): return True
 
 ##########################################################
 # Delete User
 ##########################################################
 
-# def test_delete_user(session, sample_user):
-#     """Test deleting an existing user."""
-#     Users.create_user(**sample_user)
-#     Users.delete_user(sample_user["username"])
-#     user = session.query(Users).filter_by(username=sample_user["username"]).first()
-#     assert user is None
-def test_delete_user(): return True
+def test_delete_user(session, sample_user):
+    """Test deleting an existing user."""
+    Users.create_user(**sample_user)
+    Users.delete_user(sample_user["username"])
+    user = session.query(Users).filter_by(username=sample_user["username"]).first()
+    assert user is None
+# def test_delete_user(): return True
 
-# def test_delete_user_not_found(session):
-#     """Test deleting a non-existent user."""
-#     with pytest.raises(ValueError, match="User nonexistentuser not found"):
-#         Users.delete_user("nonexistentuser")
-def test_delete_user_not_found(): return True
+def test_delete_user_not_found(session):
+    """Test deleting a non-existent user."""
+    with pytest.raises(ValueError, match="User nonexistentuser not found"):
+        Users.delete_user("nonexistentuser")
+# def test_delete_user_not_found(): return True
 
 ##########################################################
 # Get User
 ##########################################################
 
-# def test_get_id_by_username(session, sample_user):
-#     """Test successfully retrieving a user's ID by their username."""
-#     Users.create_user(**sample_user)
-#     user_id = Users.get_id_by_username(sample_user["username"])
-#     user = session.query(Users).filter_by(username=sample_user["username"]).first()
-#     assert user.id == user_id
-def test_get_id_by_username(): return True
+def test_get_id_by_username(session, sample_user):
+    """Test successfully retrieving a user's ID by their username."""
+    Users.create_user(**sample_user)
+    user_id = Users.get_id_by_username(sample_user["username"])
+    user = session.query(Users).filter_by(username=sample_user["username"]).first()
+    assert user.id == user_id
+# def test_get_id_by_username(): return True
 
-# def test_get_id_by_username_user_not_found(session):
-#     """Test failure when retrieving a non-existent user's ID."""
-#     with pytest.raises(ValueError, match="User nonexistentuser not found"):
-#         Users.get_id_by_username("nonexistentuser")
-def test_get_id_by_username_user_not_found(): return True
+def test_get_id_by_username_user_not_found(session):
+    """Test failure when retrieving a non-existent user's ID."""
+    with pytest.raises(ValueError, match="User nonexistentuser not found"):
+        Users.get_id_by_username("nonexistentuser")
+# def test_get_id_by_username_user_not_found(): return True
